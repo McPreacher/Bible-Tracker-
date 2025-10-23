ec5a9d18f0b5f30bd8b12932ebebbf71d7c344c5
McPreacher <pastorneal84@gmail.com>
2025-10-23 11:18:49 -0400

    Add files via upload

---
+++ sw-v10.js
@@ -0,0 +1,25 @@
+
+// sw-v10.js
+const CACHE = 'bta-cache-v10';
+const ASSETS = ['./','./index.html?v=10','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE) && caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request, { ignoreSearch: false }).then(r => r || fetch(event.request)));
+});
